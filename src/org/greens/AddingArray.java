ba1237549967d77df711e73f673b0dc904b33833
vignesh532 <vigneshkmr532@gmail.com>
2019-07-01 12:39:22 +0530

    newly added code

---
+++ src/org/greens/AddingArray.java
@@ -0,0 +1,29 @@
+package org.greens;
+
+public class AddingArray {
+	
+	static int k;
+	
+	static int a[] = { 1,2,3,4,5,6,7,8,9,10};
+	
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < a.length; i++) {
+			
+			k += a[i];
+			
+		}
+		
+		System.out.println(k);
+						
+		}
+		
+		}
+		
+		
+		
+	
+	
+	
+
+
