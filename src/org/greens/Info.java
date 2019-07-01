adbe6bade80027762a0209b649ac731cfb216305
vignesh532 <vigneshkmr532@gmail.com>
2019-07-01 12:56:57 +0530

    added few lines

--- src/org/greens/Info.java
+++ src/org/greens/Info.java
@@ -20,7 +20,17 @@ public class Info {
 				c.CompanyName();
 				c.CompanyId();
 				c.CompanyAddress();
+				
+			    for (int i = 1; i <= 3; i++) {
+				      for (int j = 1; j <= 3; j++) {
+				        System.out.println(i);
+				        
+				      }
+				      
+
+				    }
+				    
+				  }
 			}
 				
 		
-}
